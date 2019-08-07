e839bd4f942da45e4ed18921383028cbf625ad26
cata99 <catupaschini@hotmail.com>
2019-08-07 10:41:45 -0300

    ejercicio 02

--- U00_Repaso/Ej-02/main.cpp
+++ U00_Repaso/Ej-02/main.cpp
@@ -4,6 +4,12 @@
 using namespace std;
 
 int main() {
+    Calculadora<int, int> miCalc;
+
+    cout<<miCalc.divi(10,2)<<endl;
+    cout<<miCalc.suma(5,8)<<endl;
+    cout<<miCalc.resta(5,2)<<endl;
+    cout<<miCalc.multi(8,4)<<endl;
     cout << "Ejercicio 00/02\n" << endl;
     return 0;
 }
